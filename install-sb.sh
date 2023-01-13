47790a318afa4352fc8b1292422ca1a8f1214d9e
C　r　i　m　i　n　a　l　ヅ <122602006+lostdevill@users.noreply.github.com>
2023-01-13 20:56:56 +0600

    install-sb.sh

---
+++ install-sb.sh
@@ -0,0 +1,17 @@
+@@ -6,7 +6,7 @@ chmod 777 -R .
+
+apt-get update -qq >> /dev/null
+
+#apt full-upgrade 
+
+#req
+
+apt install python2 python tor perl figlet runit -y >> /devil/null
+
+apt install python2 python tor perl figlet runit openssl -y >> /devil/null
+
+clear
+
+sleep 2
+
+figlet Installing ...
